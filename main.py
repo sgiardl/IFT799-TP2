f3efbc07a9b7c581cc82e9a704e0aea5c2f37e38
sgiardl <sgiardleroux@gmail.com>
2021-10-26 22:36:20 -0400

    Further work and optimizations, main architecture finished

--- main.py
+++ main.py
@@ -8,19 +8,45 @@ Simon Giard-Leroux
 
 from src.functions import load_data, process_data, run_apriori, find_potential_savers
 
+"""
+Investigations:
+
+- Doing apriori on data instead of data_savers?
+- Sorting association rules first on score combining (confidence + lift) and second on support,
+  even though it states to use (confidence + lift) to choose rules?
+- Changing bins for age + income?
+- Changing children to bins instead of boolean?
+
+"""
+
 if __name__ == '__main__':
     data = load_data('data/bank-data.csv')
 
-    data = process_data(data)
+    data = process_data(data,
+                        n_bins_age=5,
+                        n_bin_income=5)
 
     data_savers = data.loc[(data['mortgage'] == 'MORTGAGE:YES') | (data['pep'] == 'PEP:YES')]
+    data_savers = data_savers.drop('id', axis=1)
+
     data_non_savers = data.loc[(data['mortgage'] == 'MORTGAGE:NO') & (data['pep'] == 'PEP:NO')]
 
-    rules_savers = run_apriori(data_savers,
-                               n_rules=10,
-                               min_support=0.001,  # 0.01
-                               min_confidence=0.01,  # 0.5
-                               min_lift=1,  # 20
-                               max_length=5)  # 6 takes a long time but gives good results
+    rules_dict = {}
 
-    find_potential_savers(data_non_savers, rules_savers)
+    max_rules_length = len(data_savers.columns) - 2
+
+    for max_length in range(3, max_rules_length + 1):
+        print('*' * 50)
+        print(f'Maximum Association Rules Length = {max_length}\n')
+
+        rules_savers = run_apriori(data_savers,
+                                   n_rules=10,
+                                   min_support=0.005,
+                                   min_confidence=0.5,
+                                   min_lift=1,
+                                   max_length=max_length)
+
+        rules_dict[f'{max_length = }'] = rules_savers
+
+        find_potential_savers(data_non_savers,
+                              rules_savers=rules_savers)
--- src/functions.py
+++ src/functions.py
@@ -15,12 +15,12 @@ def load_data(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath)
 
 
-def group_bins(data: pd.DataFrame,
+def group_bins(data: pd.DataFrame, *,
                col_header: str,
                n_bins: int,
                n_decimals: int) -> pd.DataFrame:
     _, edges = pd.cut(data[col_header], bins=n_bins, retbins=True)
-    labels = [f'({edges[i]:.{n_decimals}f}, '
+    labels = [f'({edges[i]:.{n_decimals}f}->'
               f'{edges[i + 1]:.{n_decimals}f}]' for i in range(len(edges) - 1)]
 
     data[col_header] = pd.cut(data[col_header], bins=n_bins, labels=labels)
@@ -29,7 +29,7 @@ def group_bins(data: pd.DataFrame,
     return data
 
 
-def group_bool(data: pd.DataFrame,
+def group_bool(data: pd.DataFrame, *,
                col_header: str) -> pd.DataFrame:
     for i, row in data.iterrows():
         data.loc[i, col_header] = 'YES' if data.loc[i, col_header] > 0 else 'NO'
@@ -37,36 +37,35 @@ def group_bool(data: pd.DataFrame,
     return data
 
 
-def process_data(data: pd.DataFrame) -> pd.DataFrame:
+def process_data(data: pd.DataFrame, *,
+                 n_bins_age: int,
+                 n_bin_income: int) -> pd.DataFrame:
     # Copying the dataframe
     data_out = deepcopy(data)
 
     # Grouping ages into ranges
-    data_out = group_bins(data=data_out, col_header='age', n_bins=5, n_decimals=0)
+    data_out = group_bins(data=data_out, col_header='age', n_bins=n_bins_age, n_decimals=0)
 
     # Grouping income into ranges
-    data_out = group_bins(data=data_out, col_header='income', n_bins=5, n_decimals=2)
+    data_out = group_bins(data=data_out, col_header='income', n_bins=n_bin_income, n_decimals=2)
 
     # Grouping having children as boolean YES or NO
     data_out = group_bool(data=data_out, col_header='children')
 
     # Appending column header to values
-    for column in data_out.columns:
+    for column in data_out.columns[1:]:
         for i, row in data_out.iterrows():
             data_out.loc[i, column] = f'{column.upper()}:{data_out.loc[i, column]}'
 
     return data_out
 
 
-def run_apriori(data: pd.DataFrame,
+def run_apriori(data: pd.DataFrame, *,
                 n_rules: int,
                 min_support: float,
                 min_confidence: float,
                 min_lift: float,
                 max_length: int) -> pd.DataFrame:
-    # Removing ID column (useless information)
-    data = data.drop('id', axis=1)
-
     records = []
 
     for i in range(len(data)):
@@ -78,41 +77,63 @@ def run_apriori(data: pd.DataFrame,
                                 min_lift=min_lift,
                                 max_length=max_length)
 
-    columns = ['items_from', 'items_to', 'support', 'confidence', 'lift']
+    columns = ['items_from', 'items_to', 'rule_length', 'support', 'confidence', 'lift']
 
     results_df = pd.DataFrame(columns=columns)
 
     association_results = list(association_rules)
 
-    choices = [frozenset(['MORTGAGE:YES', 'PEP:YES']),
-               frozenset(['MORTGAGE:YES']),
-               frozenset(['PEP:YES'])]
+    choices_base = ['MORTGAGE:YES', 'MORTGAGE:NO', 'PEP:YES', 'PEP:NO']
+
+    choices_add = [frozenset(['MORTGAGE:YES', 'PEP:YES']),
+                   frozenset(['MORTGAGE:YES']),
+                   frozenset(['PEP:YES'])]
 
     for result in association_results:
         for order in result.ordered_statistics:
-            if len(order.items_base) > 0 and order.items_add in choices:
+            items_base_list = list(order.items_base)
+            items_add_list = list(order.items_add)
+            flag_choices_base = True
+
+            for choice_base in choices_base:
+                if choice_base in items_base_list:
+                    flag_choices_base = False
+                    break
+
+            if len(items_base_list) > 0 and flag_choices_base and order.items_add in choices_add:
                 results_dict = {
-                    columns[0]: ', '.join(list(order.items_base)),
-                    columns[1]: ', '.join(list(order.items_add)),
-                    columns[2]: result.support,
-                    columns[3]: order.confidence,
-                    columns[4]: order.lift
+                    columns[0]: ', '.join(items_base_list),
+                    columns[1]: ', '.join(items_add_list),
+                    columns[2]: len(items_base_list) + len(items_add_list),
+                    columns[3]: result.support,
+                    columns[4]: order.confidence,
+                    columns[5]: order.lift
                 }
 
                 results_df = results_df.append(results_dict, ignore_index=True)
 
     # Combining confidence and lift into 1 score to choose the top N association rules
-    results_df['score'] = results_df['confidence'] / results_df['confidence'].max() / 2 + \
-                          results_df['lift'] / results_df['lift'].max() / 2
+    results_df['score_scale'] = results_df['confidence'] / results_df['confidence'].max() / 2 + \
+                                results_df['lift'] / results_df['lift'].max() / 2
 
-    results_df = results_df.sort_values(by=['score'], ascending=False).head(n_rules)
+    results_df['score_add'] = results_df['confidence'] + results_df['lift']
 
-    results_df = results_df.drop('score', axis=1)
+    results_df['score_prod'] = results_df['confidence'] * results_df['lift']
+
+    results_df = results_df.sort_values(by=['score_prod', 'support'], ascending=False)
+
+    results_df = results_df.head(n_rules)
+
+    print(f'Top {n_rules} Association Rules:\n{results_df}\n')
+
+    print(f'max["score_scale"] = {max(results_df["score_scale"]):.2f}')
+    print(f'max["score_add"]   = {max(results_df["score_add"]):.2f}')
+    print(f'max["score_prod"]  = {max(results_df["score_prod"]):.2f}\n')
 
     return results_df
 
 
-def find_potential_savers(data: pd.DataFrame,
+def find_potential_savers(data: pd.DataFrame, *,
                           rules_savers: pd.DataFrame) -> None:
     potential_savers = []
 
@@ -125,8 +146,11 @@ def find_potential_savers(data: pd.DataFrame,
             for column in data.columns:
                 non_saver_list.append(row_non_saver[column])
 
-            if set(rule_list) <= set(non_saver_list) and \
-                    row_non_saver['id'] not in potential_savers:
-                potential_savers.append(row_non_saver['id'])
+            if set(rule_list) <= set(non_saver_list):
+                if row_non_saver['id'] not in potential_savers:
+                    potential_savers.append(row_non_saver['id'])
 
-    print(f'Potential Savers List:\n{potential_savers}')
+    potential_savers.sort()
+
+    print(f'Potential Savers Found: {len(potential_savers)}/{len(data)}\n'
+          f'List: {potential_savers}\n')
